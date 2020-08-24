5624ed0246ecbcfd09ecaba038166df01dcfdca4
zhengyu <yu_zheng@jsxzkingdee.com>
2020-08-24 17:56:24 +0800

    使用组件form表单

--- pages/register/register.vue
+++ pages/register/register.vue
@@ -1,12 +1,30 @@
 <template>
 	<view class="main-container">
-		<view class="box">
+		<u-form :model="form" ref="uForm">
+			<u-form-item label="姓名"><u-input v-model="form.name" /></u-form-item>
+			<u-form-item label="简介"><u-input v-model="form.pwd" /></u-form-item>
+			<u-form-item label="电话"><u-input v-model="form.tel" /></u-form-item>
+			<u-form-item label="属性">
+				<u-radio-group v-model="radio">
+					<u-radio v-for="(item, index) in radioList" :key="index" :name="item.name" :disabled="item.disabled">
+						{{ item.name }}
+					</u-radio>
+				</u-radio-group>
+			</u-form-item>
+			<u-form-item>
+				<u-picker mode="region" v-model="show" :area-code='["13", "1303", "130304"]'></u-picker>
+			</u-form-item>
+			
+			<button class="btn" type="primary" @click="regist(form.name,form.pwd)">注册</button>
+		</u-form>
+		
+		<!-- <view class="box">
 			<text>用户名：</text>
 			<input placeholder="输入用户名" v-model="username"/>
 			<text>密码：</text>
 			<input placeholder="输入用户密码" v-model="password"/>
 			<button class="btn" type="primary" @click="regist(username,password)">注册</button>
-		</view>
+		</view> -->
 	</view>
 </template>
 
@@ -15,7 +33,24 @@
 		data(){
 			return {
 				username:'mario',
-				password:'123456'
+				password:'123456',
+				form: {
+					name: '',
+					pwd: '',
+					tel: ''
+				},
+				radioList: [
+					{
+						name: '个人',
+						disabled: false
+					},
+					{
+						name: '法人',
+						disabled: false
+					}
+				],
+				radio: '',
+				show: true
 			}
 		},
 		methods:{
